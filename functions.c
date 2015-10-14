f65c12fdf0245d624762f51d1abf547af64208de
donothingloop <donothingloop@gmail.com>
2015-10-14 16:32:43 +0200

    Fixed json output syntax

--- functions.c
+++ functions.c
@@ -1252,12 +1252,12 @@ int print_dhinfo(int pkt_type)
                                 "\"result\":\"info\","
                                 "\"result-type\":\"ack\","
                                 "\"result-ip\":\"%s\","
-                                "\"result-next-srv\":\"%s\",", 
+                                "\"result-next-srv\":\"%s\"", 
                                 get_ip_str(dhcph_g->dhcp_yip), 
                                 get_ip_str(dhcph_g->dhcp_sip));
 
                         if(dhcph_g->dhcp_gip) {
-                                fprintf(stdout, "\"result-relay-agent\":\"%s\"", get_ip_str(dhcph_g->dhcp_gip));
+                                fprintf(stdout, ",\"result-relay-agent\":\"%s\"", get_ip_str(dhcph_g->dhcp_gip));
                         }
 
                         fprintf(stdout, "}");
